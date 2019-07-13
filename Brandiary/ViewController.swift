f76e6379165250a1f873421916a1897397b4080a
young <fomagran6@naver.com>
2019-07-13 15:47:37 +0900

    Initial Commit

---
+++ Brandiary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Brandiary
+//
+//  Created by young on 13/07/2019.
+//  Copyright © 2019 young. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
